25e51caf56be592dc45b16d7c1b7f1544281b469
DippyArtu <artur.makhnach@gmail.com>
2020-11-06 16:51:56 +0300

    upd

---
+++ test.bat
@@ -0,0 +1,70 @@
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (1).png'"
+call rename solution.svg solution1.svg
+call move solution1.svg ..\SOLUTIONS\
+call rename maze.svg maze1.svg
+call move maze1.svg ..\MAZES\
+echo "1 GENERATED"
+
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (2).png'"
+call rename solution.svg solution2.svg
+call move solution2.svg ..\SOLUTIONS\
+call rename maze.svg maze2.svg
+call move maze2.svg ..\MAZES\
+echo "2 GENERATED"
+
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (3).png'"
+call rename solution.svg solution3.svg
+call move solution3.svg ..\SOLUTIONS\
+call rename maze.svg maze3.svg
+call move maze3.svg ..\MAZES\
+echo "3 GENERATED"
+
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (4).png'"
+call rename solution.svg solution4.svg
+call move solution4.svg ..\SOLUTIONS\
+call rename maze.svg maze4.svg
+call move maze4.svg ..\MAZES\
+echo "4 GENERATED"
+
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (5).png'"
+call rename solution.svg solution5.svg
+call move solution5.svg ..\SOLUTIONS\
+call rename maze.svg maze5.svg
+call move maze5.svg ..\MAZES\
+echo "5 GENERATED"
+
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (6).png'"
+call rename solution.svg solution6.svg
+call move solution6.svg ..\SOLUTIONS\
+call rename maze.svg maze6.svg
+call move maze6.svg ..\MAZES\
+echo "6 GENERATED"
+
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (7).png'"
+call rename solution.svg solution7.svg
+call move solution7.svg ..\SOLUTIONS\
+call rename maze.svg maze7.svg
+call move maze7.svg ..\MAZES\
+echo "7 GENERATED"
+
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (8).png'"
+call rename solution.svg solution8.svg
+call move solution8.svg ..\SOLUTIONS\
+call rename maze.svg maze8.svg
+call move maze8.svg ..\MAZES\
+echo "8 GENERATED"
+
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (9).png'"
+call rename solution.svg solution9.svg
+call move solution9.svg ..\SOLUTIONS\
+call rename maze.svg maze9.svg
+call move maze9.svg ..\MAZES\
+echo "9 GENERATED"
+
+call gradlew.bat run --args="--size=60,60 --mask ..\mask\'mask (10).png'"
+call rename solution.svg solution10.svg
+call move solution10.svg ..\SOLUTIONS\
+call rename maze.svg maze10.svg
+call move maze10.svg ..\MAZES\
+echo "10 GENERATED"
+
